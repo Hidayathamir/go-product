78a72841033dc101bbbd82ff4e715d1cb01b82d7
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-25 14:16:33 +0700

    feat: implement repo stock

--- internal/repo/stock.go
+++ internal/repo/stock.go
@@ -2,9 +2,13 @@ package repo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Hidayathamir/go-product/config"
 	"github.com/Hidayathamir/go-product/internal/repo/db"
+	"github.com/Hidayathamir/go-product/internal/repo/db/entity/table"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
 )
 
 //go:generate mockgen -source=stock.go -destination=mockrepo/stock.go -package=mockrepo
@@ -34,11 +38,51 @@ func NewStock(cfg config.Config, db *db.Postgres) *Stock {
 }
 
 // IncrementStock implements IStock.
-func (s *Stock) IncrementStock(context.Context, int64) error {
-	panic("unimplemented")
+func (s *Stock) IncrementStock(ctx context.Context, productID int64) error {
+	sql, args, err := s.db.Builder.
+		Update(table.Stock.String()).
+		Set(table.Stock.Stock, sq.Expr(table.Stock.Stock+"+1")).
+		Where(sq.Eq{
+			table.Stock.ProductID: productID,
+		}).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("Stock.db.Builder.ToSql: %w", err)
+	}
+
+	commandTag, err := s.db.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("Stock.db.Pool.Exec: %w", err)
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("pgconn.CommandTag.RowsAffected == 0: %w", pgx.ErrNoRows)
+	}
+
+	return nil
 }
 
 // DecrementStock implements IStock.
-func (s *Stock) DecrementStock(context.Context, int64) error {
-	panic("unimplemented")
+func (s *Stock) DecrementStock(ctx context.Context, productID int64) error {
+	sql, args, err := s.db.Builder.
+		Update(table.Stock.String()).
+		Set(table.Stock.Stock, sq.Expr(table.Stock.Stock+"-1")).
+		Where(sq.Eq{
+			table.Stock.ProductID: productID,
+		}).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("Stock.db.Builder.ToSql: %w", err)
+	}
+
+	commandTag, err := s.db.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("Stock.db.Pool.Exec: %w", err)
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("pgconn.CommandTag.RowsAffected == 0: %w", pgx.ErrNoRows)
+	}
+
+	return nil
 }
