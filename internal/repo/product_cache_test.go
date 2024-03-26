195485c7615c8b9d2c3eb4421883e648f3ab3dd9
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-26 22:32:08 +0700

    test: unit test repo product cache get detail by sku

--- internal/repo/product_cache_test.go
+++ internal/repo/product_cache_test.go
@@ -95,3 +95,85 @@ func TestUnitProductCacheGetDetailByID(t *testing.T) {
 		require.ErrorContains(t, err, "json.Unmarshal")
 	})
 }
+
+func TestUnitProductCacheGetDetailBySKU(t *testing.T) {
+	t.Parallel()
+
+	t.Run("get detail by sku success", func(t *testing.T) {
+		t.Parallel()
+
+		s := miniredis.RunT(t)
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr: s.Addr(),
+		})
+
+		p := &ProductCache{
+			cfg: config.Config{},
+			rdb: rdb,
+		}
+
+		expectedProduct := goproduct.ResProductDetail{
+			ID:          234231,
+			SKU:         "asdf",
+			Slug:        "aefsf",
+			Name:        "fsdfea",
+			Description: "asdfes",
+			Stock:       234,
+			CreatedAt:   time.Time{},
+			UpdatedAt:   time.Time{},
+		}
+
+		err := s.Set(p.keyDetailBySKU(expectedProduct.SKU), jutil.ToJSONString(expectedProduct))
+		require.NoError(t, err)
+
+		product, err := p.GetDetailBySKU(context.Background(), expectedProduct.SKU)
+
+		require.NoError(t, err)
+		assert.Equal(t, expectedProduct, product)
+	})
+	t.Run("key not found should return error", func(t *testing.T) {
+		t.Parallel()
+
+		s := miniredis.RunT(t)
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr: s.Addr(),
+		})
+
+		p := &ProductCache{
+			cfg: config.Config{},
+			rdb: rdb,
+		}
+
+		product, err := p.GetDetailBySKU(context.Background(), "dummy sku")
+
+		assert.Empty(t, product)
+		require.Error(t, err)
+		require.ErrorIs(t, err, redis.Nil)
+	})
+	t.Run("json marshal error should return error", func(t *testing.T) {
+		t.Parallel()
+
+		s := miniredis.RunT(t)
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr: s.Addr(),
+		})
+
+		p := &ProductCache{
+			cfg: config.Config{},
+			rdb: rdb,
+		}
+
+		sku := "dummysku"
+		err := s.Set(p.keyDetailBySKU(sku), "plain string will error json unmarshal to struct")
+		require.NoError(t, err)
+
+		product, err := p.GetDetailBySKU(context.Background(), sku)
+
+		assert.Empty(t, product)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "json.Unmarshal")
+	})
+}
