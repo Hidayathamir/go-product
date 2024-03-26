9e41bb390232ddfd9e882e36632deafdc6fe6b71
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-26 14:03:59 +0700

    test: controller grpc base unit test search

---
+++ internal/controller/grpc/stock_unit_test.go
@@ -0,0 +1,17 @@
+package grpc
+
+import "testing"
+
+func TestUnitStockIncrementStock(t *testing.T) {
+	t.Parallel()
+
+	t.Run("increment success", func(t *testing.T) { t.Parallel() })
+	t.Run("usecase error should return error", func(t *testing.T) { t.Parallel() })
+}
+
+func TestUnitStockDecrementStock(t *testing.T) {
+	t.Parallel()
+
+	t.Run("decrement success", func(t *testing.T) { t.Parallel() })
+	t.Run("usecase error should return error", func(t *testing.T) { t.Parallel() })
+}
