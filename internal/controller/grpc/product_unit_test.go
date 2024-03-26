105ef6a2472a3e6be1e8695da4624a359dd9e292
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-26 13:15:24 +0700

    test: controller grpc base unit test

---
+++ internal/controller/grpc/product_unit_test.go
@@ -0,0 +1,17 @@
+package grpc
+
+import "testing"
+
+func TestUnitProductSearch(t *testing.T) {
+	t.Parallel()
+
+	t.Run("product search success", func(t *testing.T) { t.Parallel() })
+	t.Run("product search usecaes error should return error", func(t *testing.T) { t.Parallel() })
+}
+
+func TestUnitProductGetDetail(t *testing.T) {
+	t.Parallel()
+
+	t.Run("get product success", func(t *testing.T) { t.Parallel() })
+	t.Run("get product usecaes error should return error", func(t *testing.T) { t.Parallel() })
+}
