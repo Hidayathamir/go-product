042c9a5d67e98a69ddaad333494262d8333a130d
Hidayat Hamir <hidayat.03@erajaya.com>
2024-03-27 12:08:03 +0700

    refactor: fix path

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -11,7 +11,7 @@ import (
 // handleCommandLineArgsMigrate do db migration.
 func handleCommandLineArgsMigrate(cfg config.Config, arg cliArg) {
 	if arg.isIncludeMigrate {
-		schemaMigrationPath := filepath.Join("internal", "repo", "db", "schema_migration")
+		schemaMigrationPath := filepath.Join("internal", "repo", "repopostgres", "db", "schema_migration")
 		err := db.MigrateUp(cfg, schemaMigrationPath)
 		if err != nil {
 			logrus.Fatalf("db.MigrateUp: %v", err)
